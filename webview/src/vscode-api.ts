a9f6ad4bb1183a77c57f2a74c1fbb0d53a5d017e
Juan Cruz Viotti <jv@jviotti.com>
2025-11-13 12:41:33 -0400

    `studio-isolate-vscodepostmessage-in-vscode-apits-adversarial-1`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- webview/src/vscode-api.ts
+++ webview/src/vscode-api.ts
@@ -1,26 +1,47 @@
+export type TabType = 'lint' | 'format' | 'metaschema';
+
+interface VSCodeAPI {
+  postMessage(message: unknown): void;
+  getState(): unknown;
+  setState(state: unknown): void;
+}
+
 declare global {
   interface Window {
-    acquireVsCodeApi: () => {
-      postMessage(message: unknown): void;
-      getState(): unknown;
-      setState(state: unknown): void;
-    };
+    acquireVsCodeApi: () => VSCodeAPI;
   }
 }
 
 class VSCodeAPIWrapper {
   private readonly vsCodeApi = window.acquireVsCodeApi();
 
-  public postMessage(message: unknown): void {
+  private postMessage(message: unknown): void {
     this.vsCodeApi.postMessage(message);
   }
 
-  public getState(): unknown {
-    return this.vsCodeApi.getState();
+  public openExternal(url: string): void {
+    // TODO: Implement this
+    console.log('openExternal called with:', url);
   }
 
-  public setState(state: unknown): void {
-    this.vsCodeApi.setState(state);
+  public formatSchema(): void {
+    // TODO: Implement this
+    console.log('formatSchema called');
+  }
+
+  public goToPosition(position: [number, number, number, number]): void {
+    // TODO: Implement this
+    console.log('goToPosition called with:', position);
+  }
+
+  public getActiveTab(): TabType | undefined {
+    // TODO: Implement this properly
+    return 'lint';
+  }
+
+  public setActiveTab(tab: TabType): void {
+    // TODO: Implement this
+    console.log('setActiveTab called with:', tab);
   }
 }
 
